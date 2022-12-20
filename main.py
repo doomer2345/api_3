0ac684f4836baedb50d6c1db4582fa798e19cff4
doomer2345 <evpalov.daniil1996@gmail.com>
2022-12-20 10:39:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import os
+from urllib.parse import urlparse
+
+import requests
+from dotenv import load_dotenv
+import argparse
+
+def shorten_link(api_key, user_url):
+
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+
+    payload = {
+        "long_url": user_url
+    }
+
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    return response.json()["link"]
+
+
+def count_clicks(api_key, user_url):
+
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{user_url}/clicks/summary'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()["total_clicks"]
+
+
+def is_bitlink(user_url):
+    headers = {
+        'Authorization': f'Bearer {api_key}'
+    }
+
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{user_url}'
+    response = requests.get(url, headers=headers)
+    return response.ok
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    api_key = os.environ['BITLY_APIKEY']
+    parser = argparse.ArgumentParser(
+        description='программа сокращает ссылки и считает клики '
+    )
+    parser.add_argument('link', help='Ваша ссылка')
+    args = parser.parse_args()
+    print(args.link)
+    user_url = args.link
+    parsed_url = urlparse(user_url)
+    url_without_scheme = f"{parsed_url.netloc}{parsed_url.path}"
+
+    
+    try:
+        if is_bitlink(user_url):
+            print('Количество кликов', count_clicks(api_key, url_without_scheme))
+        else:
+            print('Битлинк', shorten_link(api_key, user_url))
+    except requests.exceptions.HTTPError:
+        print("Введена неверная ссылка")
